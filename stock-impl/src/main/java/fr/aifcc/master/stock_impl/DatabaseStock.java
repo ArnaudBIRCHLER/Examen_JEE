036367d091e7a09dc8645fc90510c70f6c2dd7e4
Sébastien <sebastien-guignard@orange.fr>
2017-06-13 11:58:39 +0200

    Implémentation de la couche BDD en cours

---
+++ stock-impl/src/main/java/fr/aifcc/master/stock_impl/DatabaseStock.java
@@ -0,0 +1,203 @@
+
+package fr.aifcc.master.stock_impl;
+
+import java.util.*;
+import java.sql.*;
+
+import fr.aifcc.master.stock_api.*;
+
+/**
+ * Implémentation concrète de la classe d'accès aux données.
+ * La technologie de stockage choisie est une base de données classique.
+ * @author PIVARD Julien
+ */
+public class DatabaseStock implements StockInterface    
+{
+
+    /**
+     * L'objet de connections à la base de données.
+     * */
+    private final Connection connection;
+
+    /**
+     * Le nom des différentes table de la base de données.
+     * */
+    private final String TABLE_NAME = "Denree";
+
+    /**
+     * Les noms des différents champs de la base de données.
+     * */
+    private final String TABLE_ID = "ID";
+    private final String NAME = "nom";
+    private final String CATEGORTIE = "categorie";
+    private final String QUANTITY = "quantite";
+
+
+    /**
+     * Initialise une connexion à la base de données
+     * @param driverClass
+     * le nom du driver de la BDD.
+     * @param databaseURL
+     * l'URL vers la base de données.
+     * */
+    public DatabaseStock(String driverClass, String dataBaseUrl, String user, String pswd) throws StockException{
+        try{
+            Class.forName(driverClass);
+            this.connection = DriverManager.getConnection(dataBaseUrl, user, pswd);     
+        }
+        catch(ClassNotFoundException notFound){
+            throw new StockException("Le driver de connexion n'existe pas !!!");
+        }
+        catch(SQLException sqlDefault){
+            throw new StockException("Tu n'y connais rien en SQL ou quoi ???");
+        }
+    }
+
+    @Override
+    public synchronized Collection<Denree> getListeDenree( long offset, long limit ) throws StockException{
+
+        Collection<Denree> denrees =  new ArrayList<Denree>() ;
+        ResultSet res;
+        String requete  = " ";
+
+
+        try{
+            requete = "SELECT * FROM " + TABLE_NAME +" LIMIT ?,?" ;
+
+            PreparedStatement statement = connection.prepareStatement(requete); 
+            statement.setLong(1, offset);
+            statement.setLong(2, limit);
+            res = statement.executeQuery(); 
+
+            if(!res.next()){
+                res.close();
+                throw new StockException("C'est quoi ce bordel \n Y'a rien dans la base !!!");
+            }
+            else {
+                do{
+                    Denree denree = new Denree();
+                    denree.setId(res.getLong(1));
+                    denree.setNom(res.getString(2));
+                    denree.setCategorie(res.getString(3));
+                    denree.setQuantity(res.getInt(4));
+                    denrees.add(denree) ;
+                }
+                while(res.next());
+                res.close();
+            }
+
+        }
+        catch(SQLException sqlDefault){
+            throw new StockException("Tu n'y connais rien en SQL ou quoi ???");
+        }
+
+        return denrees ;
+    }
+
+    @Override
+    public synchronized Denree getDenree( long id ) throws StockException{
+        ResultSet res;
+        try{
+            String requete = "SELECT * FROM " + TABLE_NAME +" WHERE ID=?";      
+            PreparedStatement statement = connection.prepareStatement(requete);
+            statement.setLong(1,id);
+
+            res = statement.executeQuery();
+            int taille = res.getFetchSize();
+
+            if(!res.next()){
+                res.close();
+                throw new StockException("C'est quoi ce bordel \n Y'a rien dans la base !!!");
+            }
+            else if(taille != 0){
+                res.close();
+                throw new StockException("C'est quoi ce bordel \n Y'a trop d'éléments !!!");
+            }
+            else {
+                Denree denree = new Denree();
+                denree.setId(res.getLong(1));
+                denree.setNom(res.getString(2));
+                denree.setCategorie(res.getString(3));
+                denree.setQuantity(res.getInt(4));
+                res.close();
+                return denree ;
+            }
+        }
+        catch(SQLException sqlDefault){
+            throw new StockException("Tu n'y connais rien en SQL ou quoi ???");
+
+        }
+    }
+
+    @Override
+    public synchronized void mAjDenree( Denree denree ) throws StockException{
+        try
+        {
+            String requete = "UPDATE " + this.TABLE_NAME + " SET "
+                + this.NAME + " = ?, "  
+                + this.CATEGORTIE + " = ?, " 
+                + this.QUANTITY + " = ? WHERE "
+                + this.TABLE_ID + " = ?";
+            // Une requête doit TOUJOURS être préparé pour éviter les injections SQL.
+            PreparedStatement requeteDenree = this.connection.prepareStatement( requete );
+
+            requeteDenree.setString( 1, denree.getNom() );
+            requeteDenree.setString( 2, denree.getCategorie() );
+            requeteDenree.setInt( 3, denree.getQuantity() );
+            requeteDenree.setLong( 4, denree.getId() );
+
+            requeteDenree.executeUpdate();
+        }
+        catch ( SQLException e )
+        {
+            throw new StockException( "La mise à jour via la requête SQL à "
+                    + "échoué ou à subit une erreur \n===> " + e.getMessage() );
+        }
+    }
+
+
+    @Override
+    public synchronized void ajouterDenree( Denree denree ) throws StockException{
+        try
+        {
+            String requete = "INSERT INTO " + this.TABLE_NAME + " ( "
+                + this.TABLE_ID + ", " 
+                + this.NAME + ", "
+                + this.CATEGORTIE + ", "
+                + this.QUANTITY + " ) VALUES ( ?, ?, ?, ? )";
+            // Une requête doit TOUJOURS être préparé pour éviter les injections SQL.
+            PreparedStatement requeteDenree = this.connection.prepareStatement( requete );
+
+            
+            requeteDenree.setLong( 1, denree.getId() );
+            requeteDenree.setString( 2, denree.getNom() );
+            requeteDenree.setString( 3, denree.getCategorie() );
+            requeteDenree.setInt( 4, denree.getQuantity() );
+
+            requeteDenree.executeUpdate();
+        }
+        catch ( SQLException e )
+        {
+            throw new StockException( "Echec SQL Insertion denree\n" + e.getMessage() );
+        }
+    }
+
+
+
+
+
+    @Override
+    public synchronized void dispose()
+        throws StockException
+    {
+        try
+        {
+            this.connection.close();
+        }
+        catch ( SQLException e )
+        {
+            throw new StockException( e );
+        }
+    }
+
+}
---
+++ stock-impl/src/test/java/fr/aifcc/master/stock_impl/TestDatabaseStock.java
@@ -0,0 +1,115 @@
+package fr.aifcc.master.stock_impl;
+
+import java.sql.*;
+import java.util.*;
+
+import fr.aifcc.master.stock_api.*;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+
+/**
+ * Classe de tests unitaire de la gestion d'accès à la BDD.
+ *
+ * ATTENTION ! Cette classe ne DOIT PAS hériter de la classe « TestCase »
+ * sinon certaines annotations ne sont pas disponible et le nom méthode
+ * de test devrait toutes commencer par « test ».
+ *
+ * Si la classe hérite c'est junit 3 qui sera utilisé à la place de junit 4 !
+ * @author 
+ */
+public class TestDatabaseStock
+{
+
+    protected static Connection connection;
+    protected DatabaseStock database;
+
+    private static final String DRIVER_MYSQL = "com.mysql.jdbc.Driver";
+    private static final String URL_MYSQL = "jdbc:mysql://localhost:3306/exam_jee_test";
+    private static final String USER_MYSQL = "root";
+    private static final String USER_MYSQL_PASSWORD = "";
+
+    /**
+     * Cette annotation n'est disponible que en junit 4
+     *
+     * Cette méthode sera exécutée une seule et unique fois AVANT
+     * de commencer les tests unitaires.
+     * */
+    @BeforeClass
+    public static void setUpClass()
+        throws Exception
+    {
+        Class.forName( DRIVER_MYSQL );
+        connection = DriverManager.getConnection(URL_MYSQL,USER_MYSQL,USER_MYSQL_PASSWORD);
+
+        insererDenree( 1, "Coca-cola", "Boisson", 100 );
+        insererDenree( 2, "Café", "Boisson", 200 );
+    }
+
+    /**
+     * Cette méthode sera exécutée avant CHAQUE test unitaire.
+     * */
+    @Before
+    public void setUp()
+        throws StockException
+    {
+        this.database = new DatabaseStock(DRIVER_MYSQL, URL_MYSQL,USER_MYSQL,USER_MYSQL_PASSWORD);
+    }
+
+
+    /**
+     * Cette annotation n'est disponible que en junit 4
+     *
+     * Cette méthode sera exécutée une seule et unique fois APRÈS
+     * que tous les tests unitaires aient été faits
+     * */
+    @AfterClass
+    public static void tearDownClass()
+        throws SQLException
+    {
+
+        connection.close();
+    }
+
+
+    /**
+     * Cette méthode sera exécutée après CHAQUE test unitaire.
+     * */
+    @After
+    public void tearDown()
+        throws StockException
+    {
+        this.database.dispose();
+        this.database = null;
+    }
+
+
+    public static void insererDenree( long id, String nom, String categorie, int quantite )
+        throws SQLException
+    {
+        System.out.println( ">>>>>>>>>>>>>> INSERTION d'une entrée dans la table Denree <<<<<<<<<<<<<" );
+        String requete = "INSERT INTO Denree ( id, nom, categorie, quantite ) VALUES ( ?, ?, ?, ? )";
+        PreparedStatement insert = connection.prepareStatement( requete );
+        insert.setLong( 1, id );
+        insert.setString( 2, nom );
+        insert.setString( 3, categorie );
+        insert.setInt( 4, quantite );
+        insert.executeUpdate();
+    }
+
+    public static void supprimerDenree( long id )
+        throws SQLException
+    {
+        System.out.println( ">>>>>>>>>>>>>> SUPPRESSION d'une entrée dans la table Denree <<<<<<<<<<<<<" );
+        String requete = "DELETE FROM Denree WHERE id=?";
+        PreparedStatement insert = connection.prepareStatement( requete );
+        insert.setLong( 1, id );
+        insert.executeUpdate();
+    }
+
+}
